9f4b42083e5a7fe0b54f865ccf312ff86d5b6a3a
mayrez <maikarox@protonmail.com>
2023-05-08 21:37:01 +0100

    Add server loaders for Express and dependency injection

---
+++ src/loaders/express.ts
@@ -0,0 +1,14 @@
+import express, { Application } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
+import routes from '../routes';
+
+export default (app: Application): void => {
+  app.use(helmet());
+  app.enable('trust proxy');
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use('/api', routes());
+};
