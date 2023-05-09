d55db748a99c9a9743b77dd2524ebf211c3a35a8
mayrez <maikarox@protonmail.com>
2023-05-09 21:08:47 +0100

    Split dependency injection into files

--- src/loaders/dependencyInjector.ts
+++ src/loaders/dependencyInjector.ts
@@ -1,21 +1,9 @@
-import { Container } from 'typedi';
+import services from './services';
+import configurations from './configurations';
+import generators from './generators';
 
-import LocalConfiguration from '../configurations/strategies/Local.configuration';
-import RemoteConfiguration from '../configurations/strategies/Remote.configuration';
-import CodeService from '../services/Code.service';
-import CodeConfigReaderService from '../services/CodeConfigReader.service';
-import CodeGeneratorService from '../services/CodeGenerator.service';
-import GuidGenerator from '../generators/Guid.generator';
-import HexadecimalGenerator from '../generators/Hexadecimal.generator';
-import SequentialGenerator from '../generators/Sequential.generator';
-
-export default  () => {
-  Container.set("LocalConfiguration", new LocalConfiguration());
-  Container.set("RemoteConfiguration", new RemoteConfiguration());
-  Container.set("GuidGenerator", new GuidGenerator());
-  Container.set("HexadecimalGenerator", new HexadecimalGenerator());
-  Container.set("SequentialGenerator", new SequentialGenerator());
-  Container.set("ConfigReaderService", new CodeConfigReaderService());
-  Container.set("CodeGeneratorService", new CodeGeneratorService());
-  Container.set("CodeService", new CodeService());
+export default () => {
+  configurations();
+  generators();
+  services();
 };
---
+++ src/loaders/services.ts
@@ -0,0 +1,11 @@
+import { Container } from 'typedi';
+
+import CodeService from '../services/Code.service';
+import CodeConfigReaderService from '../services/CodeConfigReader.service';
+import CodeGeneratorService from '../services/CodeGenerator.service';
+
+export default () => {
+  Container.set("ConfigReaderService", new CodeConfigReaderService());
+  Container.set("CodeGeneratorService", new CodeGeneratorService());
+  Container.set("CodeService", new CodeService());
+};
